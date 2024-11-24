26755c58175ab8e96c6d59c2ad92ad7fa22126b6
Marcos Juan Martos Prados <martospradosmarcos@gmail.com>
2024-11-24 16:53:40 +0000

    Second commmit

--- src/js/app.js
+++ src/js/app.js
@@ -40,21 +40,19 @@ function render(variables = {}) {
           <h3>${variables.city ? variables.city : " "} ${
     variables.country ? variables.country : " "
   }</h3>
-          <ul class=${
-            variables.socialMediaPosition ? variables.socialMediaPosition : " "
-          }
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li> ${
-              variables.twitter ? variables.twitter : " "
-            }
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li> ${
-              variables.github ? variables.github : " "
-            }
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li> ${
-              variables.linkedin ? variables.linkedin : " "
-            }
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li> ${
-              variables.instagram ? variables.instagram : " "
-            }
+          <ul class="${variables.socialMediaPosition}">            
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
